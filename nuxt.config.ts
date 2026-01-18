4108534fbb626a277493d9d37a6901ef0436b504
Nabil-Y <nabil.yassine.dev@gmail.com>
2026-01-18 02:44:57 +0100

    update umami config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,7 +62,7 @@ export default defineNuxtConfig({
 		host: "https://umami.nylab.dev",
 		autoTrack: true,
 		ignoreLocalhost: true,
+		domains: ["umami.nylab.dev"],
 		logErrors: true,
-		proxy: "cloak",
 	},
 });
